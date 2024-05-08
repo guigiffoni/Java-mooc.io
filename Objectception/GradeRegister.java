51b59680bb84e3eb9f1bcfea369127b27a43502c
Guilherme Giffoni <guilherme.giffoni@rematecvarejo.com.br>
2024-05-08 17:32:21 -0300

    update

--- Objectception/GradeRegister.java
+++ Objectception/GradeRegister.java
@@ -76,15 +76,5 @@ public class GradeRegister {
 
         return examPointsSum / this.examPoints.size();
     }
-
-    @Override
-    public String toString() {
-        System.out.println("5: ");
-        System.out.println("4: ");
-        System.out.println("3: ");
-        System.out.println("2: ");
-        System.out.println("1: ");
-        System.out.println("0: ");
-    }
 }
 
